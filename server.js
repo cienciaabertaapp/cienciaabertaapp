f077767613f15814b5bc3c1112d93c4d2781f6a9
hoyamams <hoyamams@gmail.com>
2022-01-25 09:01:14 -0300

    validações formularios finalizado

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-
-const path = require('path');
 const express = require('express');
+
+const {resolve} = require('path');
 
 const app = express();
 
+app.use('/',
+    express.static(
+        resolve(
+            __dirname,
+            '.build')))
+
+app.listen(process.env.PORT || 3000, (err) => {
+
+    if(err){return console.log(err)}
+    console.log("funcionou")
+})
+
+/*
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function() {
     console.log('listening on port ', server.address().port);
-});
+});*/
