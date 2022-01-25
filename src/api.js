baba552b8f97df877fcd617e91cbff7fc78ed908
hoyamams <hoyamams@gmail.com>
2022-01-24 22:41:53 -0300

    make it better

--- src/api.js
+++ src/api.js
@@ -3,7 +3,11 @@ import {getLocalRefreshToken, getToken, isAuthenticated, TOKEN_KEY} from "./auth
 import {Redirect, Route} from "react-router-dom";
 import {React} from "react";
 
-export const CIENCIAABERTA_API_BASE_URL = "http://localhost:8084/";
+//  http://localhost:8084/
+
+// https://fierce-hollows-92158.herokuapp.com/
+
+export const CIENCIAABERTA_API_BASE_URL = "https://cienciaaberta.herokuapp.com/";
 
 const api = axios.create({
     baseURL: CIENCIAABERTA_API_BASE_URL,
