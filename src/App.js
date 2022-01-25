4736c5dadfe979a05c2b61e91c347e90898c4d4f
hoyamams <hoyamams@gmail.com>
2022-01-25 15:10:08 -0300

    ajustando rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import {React, useState,useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import DefaultComponent from './components/DefaultComponent';
