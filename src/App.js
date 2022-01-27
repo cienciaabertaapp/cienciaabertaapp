c59a41aa69cbeedfbb9e8554be8f31a8fa81da8a
hoyamams <hoyamams@gmail.com>
2022-01-27 16:13:42 -0300

    mudando nome grau maturidade

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+
 import {BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import DefaultComponent from './components/DefaultComponent';
 import HeaderComponent from './components/HeaderComponent';
