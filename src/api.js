4fa4688f5becbdf7843cdac7aea18e94d69286c3
cienciaabertaapp <105555245+cienciaabertaapp@users.noreply.github.com>
2022-12-13 18:35:52 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -12,12 +12,6 @@ export const CIENCIAABERTA_API_BASE_URL = "https://cienciaaberta.herokuapp.com/"
 const api = axios.create({
     baseURL: CIENCIAABERTA_API_BASE_URL,
     responseType: 'json', 
-    headers: {                  
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Authorization', 
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE' ,
-          'Content-Type':'application/json;charset=UTF-8'                  
-      },
 });
 
 api.interceptors.request.use(async config => {
